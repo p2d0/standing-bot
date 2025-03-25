3a37495b0cd06372789e4c01561bc0464c1d5564
patriot720 <cerkin-3@yandex.ru>
2025-03-25 10:39:51 +0300

    Add initial files

---
+++ devshell.nix
@@ -0,0 +1,28 @@
+{ pkgs }:
+
+with pkgs;
+
+# Configure your development environment.
+devshell.mkShell {
+  name = "standing_bot";
+  motd = ''
+Entered standing_bot app development environment.
+'';
+  env = [
+    {
+      name = "PKG_CONFIG_PATH";
+      value = "${openssl.dev}/lib/pkgconfig";
+    }
+  ];
+  packages = [
+    pkg-config
+    openssl.dev
+    libiconv
+    rustc
+    cargo
+    rustfmt
+    clippy
+    rust-analyzer
+    gdb
+  ];
+}
