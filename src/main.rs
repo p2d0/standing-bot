2f080d1a9f7762d51120c0d46132464d339ea1a9
patriot720 <cerkin-3@yandex.ru>
2025-03-25 17:01:10 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,10 @@
-// This example demonstrates how to deal with messages and callback queries
-// within a single dialogue.
-//
-// # Example
-// ```
-// - /start
-// - Let's start! What's your full name?
-// - John Doe
-// - Select a product:
-//   [Apple, Banana, Orange, Potato]
-// - <A user selects "Banana">
-// - John Doe, product 'Banana' has been purchased successfully!
-// ```
-
-use std::fmt::Debug;
+use std::error::Error;
 
 use chrono::{DateTime, Utc};
 use teloxide::{
     dispatching::{dialogue::{self, serializer::Json, ErasedStorage, GetChatId, SqliteStorage, Storage}, MessageFilterExt, UpdateHandler},
     prelude::*,
-    types::{ButtonRequest, ChatShared, InlineKeyboardButton, InlineKeyboardMarkup, InputFile, KeyboardButton, KeyboardButtonRequestChat, KeyboardMarkup, MessageChatShared, MessageKind, RequestId},
+    types::{ButtonRequest, InputFile, KeyboardButton, KeyboardButtonRequestChat, KeyboardMarkup, MessageChatShared, MessageKind, RequestId},
     utils::command::BotCommands,
 };
 
@@ -30,6 +16,8 @@ const STICKER_STAND: &str = "CAACAgIAAyEFAASI1aNsAAIBkGffuc0yHnECp7_WveEQPfDRxiV
 const STICKER_SIT: &str = "CAACAgIAAyEFAASI1aNsAAIBlmffuuiIMChfPKanMJKmMp_9Dg91AAKWYQACU4jwSvNuavKzs5PgNgQ";
 
 #[derive(Clone, Default, serde::Serialize, serde::Deserialize)]
+#[derive(Debug)]
+#[derive(PartialEq)]
 pub enum State {
     #[default]
     Start,
@@ -88,9 +76,9 @@ fn schema() -> UpdateHandler<Box<dyn std::error::Error + Send + Sync + 'static>>
         .branch(case![Command::Cancel].endpoint(cancel));
 
     let message_handler = Update::filter_message()
-        // .inspect(|u: Update| {
-        //     eprintln!("{u:#?}"); // Print the update to the console with inspect
-        // })
+    // .inspect(|u: Update| {
+    //     eprintln!("{u:#?}"); // Print the update to the console with inspect
+    // })
         .branch(command_handler)
         .branch(dptree::filter_map(|message: Message| {
             match message.kind {
@@ -104,39 +92,24 @@ fn schema() -> UpdateHandler<Box<dyn std::error::Error + Send + Sync + 'static>>
         .branch(dptree::endpoint(invalid_state));
 
     let sticker_handler = Update::filter_channel_post()
-        // .inspect(|u: Update| {
-        //     eprintln!("{u:#?}"); // Print the update to the console with inspect
-        // })
+    // .inspect(|u: Update| {
+    //     eprintln!("{u:#?}"); // Print the update to the console with inspect
+    // })
         .branch(Message::filter_sticker()
-                .branch(case![State::StandingChoice { chat_id }].endpoint(standing_choice_sticker))
-                .branch(case![State::ReceiveStandingCommand { chat_id , timestamp }].endpoint(start_standing_sticker))
-                .endpoint(handle_stickers));
-
-    let callback_query_handler = Update::filter_callback_query().branch(
-        case![State::ReceiveProductChoice { full_name }].endpoint(receive_product_selection),
-    );
-
-    // let shared_chat = Update::filter_message()
-    //     .branch(dptree::filter_map(|message: Message| {
-    //         match message.kind {
-    //             MessageKind::ChatShared(x) => Some(x),
-    //             _ => None,
-    //         }
-    //     }).endpoint(chat_shared));
+                .branch(case![State::ReceiveStandingCommand { chat_id , timestamp }].endpoint(receive_start_standing_sticker))
+                .endpoint(start_standing_sticker));
 
     dialogue::enter::<Update, ErasedStorage<State>, State, _>()
         .branch(sticker_handler)
         .branch(message_handler)
-        .branch(callback_query_handler)
 }
 
-async fn start(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+async fn start(bot: Bot, msg: Message) -> HandlerResult {
     bot.send_message(msg.chat.id, "Скинь чат бро")
        .reply_markup(
            KeyboardMarkup::new([[
                KeyboardButton::new("Отправить чат").request(ButtonRequest::RequestChat(KeyboardButtonRequestChat::new(RequestId(1), true))),
            ]])).await?;
-    dialogue.update(State::ReceiveFullName).await?;
     Ok(())
 }
 
@@ -145,7 +118,7 @@ async fn help(bot: Bot, msg: Message) -> HandlerResult {
     Ok(())
 }
 
-async fn handle_stickers(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+async fn start_standing_sticker(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
     match msg.sticker().map(ToOwned::to_owned) {
         Some(sticker) => {
             if sticker.file.id == STICKER_STAND {
@@ -160,7 +133,6 @@ async fn handle_stickers(bot: Bot, dialogue: MyDialogue, msg: Message) -> Handle
 }
 
 async fn chat_shared(bot: Bot, msg: Message, dialogue: MyDialogue, chat: MessageChatShared) -> HandlerResult {
-    // bot.send_message(msg.chat.id, chat.chat_shared.chat_id.to_string()).await?;
     bot.send_message(msg.chat.id, format!("Текущий чат: {}",chat.chat_shared.chat_id))
        .reply_markup(
            KeyboardMarkup::new([[
@@ -189,44 +161,24 @@ async fn standing_choice(bot: Bot, dialogue: MyDialogue, msg: Message, chat_id: 
     Ok(())
 }
 
-async fn standing_choice_sticker(bot: Bot, dialogue: MyDialogue, msg: Message, chat_id: ChatId) -> HandlerResult {
-    if let Some(sticker) = msg.sticker() {
-        if sticker.file.id == STICKER_STAND {
-            bot.send_message(chat_id, "СТОИМ БРАТЬЯ").await?;
-            dialogue.update(State::ReceiveStandingCommand {
-                chat_id,
-                timestamp: Utc::now().timestamp()
-            }).await?;
-        } else {
-            bot.send_message(chat_id, "СТОИМ БРАТЬЯ?").await?;
-        }
-    }
-    Ok(())
-}
-
-async fn start_standing_sticker(bot: Bot, dialogue: MyDialogue, msg: Message, (chat_id, timestamp): (ChatId,i64)) -> HandlerResult {
+async fn receive_start_standing_sticker(bot: Bot, dialogue: MyDialogue, msg: Message, (chat_id, timestamp): (ChatId,i64)) -> HandlerResult {
     if let Some(sticker) = msg.sticker() {
         if sticker.file.id == STICKER_SIT {
-            dialogue.update(State::StandingChoice { chat_id }).await?;
-            send_time_difference(bot, chat_id, timestamp).await?;
+            dialogue.exit().await?;
+            bot.send_message(chat_id, format!("ПОСТОЯЛИ {}",get_time_difference(timestamp).unwrap_or("".into()))).await?;
+        } else {
+            bot.send_message(chat_id, format!("СТОИМ {}",get_time_difference(timestamp).unwrap_or("".into()))).await?;
         }
     }
     Ok(())
 }
 
-async fn send_time_difference(bot: Bot, chat_id: ChatId, timestamp: i64) -> Result<(), Box<dyn Error + Send + Sync>> {
-    let now = Utc::now();
-    Ok(if let Some(past) = DateTime::from_timestamp(timestamp,0) {
-        let difference = now.signed_duration_since(past).num_minutes();
-        bot.send_message(chat_id, format!("ПРОСТОЯЛИ {difference} минут")).await?;
-    })
-}
 
 async fn start_standing(bot: Bot, dialogue: MyDialogue, msg: Message, (chat_id, timestamp): (ChatId,i64)) -> HandlerResult {
     if let Some(full_name) = msg.text().map(ToOwned::to_owned) {
         if full_name == "СИДИМ" {
             dialogue.update(State::StandingChoice { chat_id }).await?;
-            send_time_difference(bot, chat_id, timestamp).await?;
+            bot.send_message(chat_id, format!("ПОСТОЯЛИ {}",get_time_difference(timestamp).unwrap_or("".into()))).await?;
 
             bot.send_message(msg.chat.id, "СИДИМ")
                .reply_markup(KeyboardMarkup::new([[
@@ -236,6 +188,17 @@ async fn start_standing(bot: Bot, dialogue: MyDialogue, msg: Message, (chat_id, 
         }
     }
     Ok(())
+}
+
+fn get_time_difference(timestamp: i64) -> Option<String> {
+    let now = Utc::now();
+    if let Some(past) = DateTime::from_timestamp(timestamp,0) {
+        let difference = now.signed_duration_since(past);
+        let minutes = difference.num_minutes();
+        let seconds = difference.num_seconds();
+        return Some(format!("{minutes} минут {seconds} секунд"));
+    }
+    None
 }
 
 async fn cancel(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
@@ -250,56 +213,31 @@ async fn invalid_state(bot: Bot, msg: Message) -> HandlerResult {
     Ok(())
 }
 
-async fn receive_full_name(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
-    match msg.text().map(ToOwned::to_owned) {
-        Some(full_name) => {
-            let products = ["Apple", "Banana", "Orange", "Potato"]
-                .map(|product| InlineKeyboardButton::callback(product, product));
-
-            bot.send_message(msg.chat.id, "Select a product:")
-               .reply_markup(InlineKeyboardMarkup::new([products]))
-               .await?;
-            dialogue.update(State::ReceiveProductChoice { full_name }).await?;
-        }
-        None => {
-            bot.send_message(msg.chat.id, "Please, send me your full name.").await?;
-        }
-    }
-
-    Ok(())
-}
-
-async fn receive_product_selection(
-    bot: Bot,
-    dialogue: MyDialogue,
-    full_name: String, // Available from `State::ReceiveProductChoice`.
-    q: CallbackQuery,
-) -> HandlerResult {
-    if let Some(product) = &q.data {
-        bot.send_message(
-            dialogue.chat_id(),
-            format!("{full_name}, product '{product}' has been purchased successfully!"),
-        )
-           .await?;
-        dialogue.exit().await?;
-    }
-
-    Ok(())
-}
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use dptree::deps;
-    use teloxide::dispatching::{dialogue::{InMemStorage }, UpdateHandler};
+    use teloxide::dispatching::{dialogue::{InMemStorage}};
     use teloxide_tests::{MockBot, MockMessageText};
 
     #[tokio::test]
     async fn test_start_tree() {
         let bot = MockBot::new(MockMessageText::new().text("/start"), schema());
-        bot.dependencies(deps![InMemStorage::<State>::new()]);
+        let storage: MyStorage = InMemStorage::new().erase();
+        bot.dependencies(deps![storage]);
         bot.dispatch_and_check_last_text_and_state("Скинь чат бро",State::Start).await;
+        // bot.update(MockMessageText::new().text("kekes"));
+        // bot.dispatch_and_check_last_text_and_state("Your tasks: kekes",State::AskForText).await;
+    }
+
+    #[tokio::test]
+    #[ignore]
+    async fn test_stand_brothers() {
+        // let bot = MockBot::new(MockMessageText::new().text("/start"), schema());
+        // let storage: MyStorage = InMemStorage::new().erase();
+        // bot.dependencies(deps![storage]);
+        // bot.dispatch_and_check_last_text_and_state("Скинь чат бро",State::Start).await;
         // bot.update(MockMessageText::new().text("kekes"));
         // bot.dispatch_and_check_last_text_and_state("Your tasks: kekes",State::AskForText).await;
     }
