b926318e115acaa6b60921c3103d62df70a1ff4f
patriot720 <cerkin-3@yandex.ru>
2025-03-26 17:44:59 +0300

    temporarily disable periodic updates

--- src/periodic_updates.rs
+++ src/periodic_updates.rs
@@ -11,26 +11,26 @@ use crate::time::get_time_difference;
 #[derive(Clone, Default, serde::Serialize, serde::Deserialize)]
 pub struct UpdateData(pub Option<Message>, pub i64);
 
-pub async fn periodic_update_msg(bot: Bot, rx: Arc<Mutex<watch::Receiver<UpdateData>>>) {
+pub async fn periodic_update_msg(bot: Bot, rx: Arc<watch::Receiver<UpdateData>>) {
     loop {
-        let rx_guard = rx.lock().await;
-        let UpdateData(message, timestamp) = rx_guard.borrow().clone();
+        let UpdateData(message, timestamp) = rx.borrow().clone();
         if let Some(message) = message {
+            log::info!("Updating message {:#?}", message);
             let edited_message = format!("Стоим {}", get_time_difference(timestamp));
             if let Err(err) = bot.edit_message_text(message.chat_id().unwrap(),message.id,edited_message).await {
-                log::warn!("Failed to send message: {:?}", err);
+                log::warn!("Failed to update message: {:?}", err);
             };
         }
 
-        sleep(Duration::from_secs(10)).await;
+        sleep(Duration::from_secs(5)).await;
     }
 }
 
 pub async fn update_periodically(bot: Bot) -> watch::Sender<UpdateData> {
     let (tx, rx) = watch::channel(UpdateData(None, 0));
-    let rx = Arc::new(Mutex::new(rx)); // Shared state
+    let rx = Arc::new(rx); // Shared state
     let rx_clone = Arc::clone(&rx);
 
-    tokio::spawn(async move {periodic_update_msg(bot, rx_clone)}.await);
+    // tokio::spawn(async move {periodic_update_msg(bot, rx_clone)}.await);
     return tx;
 }
